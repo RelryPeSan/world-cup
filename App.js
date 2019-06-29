408af222533ecef50ee70661ba9d53b04643423d
Relry Pereira dos Santos <relrypesan@gmail.com>
2019-06-29 16:48:08 -0300

    Revert "Versão 0.1"
    This reverts commit 0eccd60bb42f112f9eadb0a2b013e97be97ded74.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Versão 0.1</Text>
+      <Text>Inicio do App!</Text>
     </View>
   );
 }
