0eccd60bb42f112f9eadb0a2b013e97be97ded74
Relry Pereira dos Santos <relrypesan@gmail.com>
2019-06-29 16:43:46 -0300

    Versão 0.1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Inicio do App!</Text>
+      <Text>Versão 0.1</Text>
     </View>
   );
 }
